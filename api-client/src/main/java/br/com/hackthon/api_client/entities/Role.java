a8244fec2b3dc4fa07f060306b654c9b1aeebf02
IgorTecnologia <iguigu2019@gmail.com>
2024-09-11 05:26:40 -0300

    Role entity

---
+++ api-client/src/main/java/br/com/hackthon/api_client/entities/Role.java
@@ -0,0 +1,21 @@
+package br.com.hackthon.api_client.entities;
+
+import com.fasterxml.jackson.annotation.*;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.*;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, length = 70, unique = true)
+    private String authority;
+}
