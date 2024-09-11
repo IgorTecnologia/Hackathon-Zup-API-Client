9a397b229c96e03f11ccbfac61095aa030bc465c
IgorTecnologia <iguigu2019@gmail.com>
2024-09-11 05:32:59 -0300

    User Service Impl

---
+++ api-client/src/main/java/br/com/hackthon/api_client/services/impl/UserServiceImpl.java
@@ -0,0 +1,164 @@
+package br.com.hackthon.api_client.services.impl;
+
+import br.com.hackthon.api_client.dto.*;
+import br.com.hackthon.api_client.entities.*;
+import br.com.hackthon.api_client.repositories.*;
+import br.com.hackthon.api_client.services.*;
+import br.com.hackthon.api_client.services.exceptions.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.*;
+
+import java.time.*;
+import java.util.*;
+import java.util.stream.*;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public Page<UserDTO> findAllPaged(Pageable pageable) {
+
+        Page<User> page = repository.findAll(pageable);
+        return page.map(x -> new UserDTO(x, x.getRoles()));
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<UserDTO> findByUsername(String userName) {
+
+        List<User> list = repository.findByUsernameContainingIgnoreCase(userName);
+        if(list.isEmpty()) {
+            throw new ResourceNotFoundException("Username not found: " + userName);
+        }
+        return list.stream().map(x -> new UserDTO(x, x.getRoles())).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<UserDTO> findByEmail(String email) {
+
+        List<User> list = repository.findByEmailContainingIgnoreCase(email);
+        if(list.isEmpty()){
+            throw new ResourceNotFoundException("Email not found: " + email);
+        }
+        return list.stream().map(x -> new UserDTO(x, x.getRoles())).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<UserDTO> findByCpf(String cpf) {
+
+        List<User> list = repository.findByCpfContainingIgnoreCase(cpf);
+        if(list.isEmpty()){
+            throw new ResourceNotFoundException("CPF not found: " + cpf);
+        }
+        return list.stream().map(x -> new UserDTO(x, x.getRoles())).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserDTO findById(UUID id) {
+
+        Optional<User> obj = repository.findById(id);
+        User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + id));
+        return new UserDTO(entity, entity.getRoles());
+    }
+
+    @Override
+    public UserDTO insert(UserDTO dto) {
+
+        User entity = new User();
+        copyDtoToEntity(entity, dto);
+        entity.setCollectionDate(LocalDateTime.now(ZoneId.of("UTC")));
+        entity.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        repository.save(entity);
+        return new UserDTO(entity, entity.getRoles());
+    }
+
+    @Override
+    public UserDTO update(UUID id, UserDTO dto) {
+
+        Optional<User> obj = repository.findById(id);
+        User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + id));
+        entity.setFullName(dto.getFullName());
+        entity.setPhoneNumber(dto.getPhoneNumber());
+        entity.setCpf(dto.getCpf());
+        entity.setImageUrl(dto.getImageUrl());
+        entity.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        repository.save(entity);
+
+        return new UserDTO(entity, entity.getRoles());
+    }
+
+    @Override
+    public UserDTO passwordUpdate(UUID id, UserDTO dto) {
+
+        Optional<User> obj = repository.findById(id);
+        if(!obj.isPresent()){
+            throw new ResourceNotFoundException(("Id not found: ") + id);
+        }if(!obj.get().getPassword().equals(dto.getOldPassword())){
+            throw new ResourceNotFoundException(("Error: Mismatched old password!"));
+        }else{
+            User entity = obj.get();
+            entity.setPassword(dto.getPassword());
+            entity.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+            repository.save(entity);
+
+            return new UserDTO(entity);
+        }
+
+    }
+
+
+    @Override
+    public void deleteById(UUID id) {
+
+        Optional<User> obj = repository.findById(id);
+        if (obj.isEmpty()) {
+            throw new ResourceNotFoundException("Id not found: " + id);
+        }
+        repository.deleteById(id);
+    }
+
+    void copyDtoToEntity(User entity, UserDTO dto) {
+
+        entity.setUsername(dto.getUsername());
+        entity.setEmail(dto.getEmail());
+        entity.setPassword(dto.getPassword());
+        entity.setFullName(dto.getFullName());
+        entity.setPhoneNumber(dto.getPhoneNumber());
+        entity.setCpf(dto.getCpf());
+        entity.setImageUrl(dto.getImageUrl());
+
+        entity.getRoles().clear();
+
+        if (dto.getRoles() != null && !dto.getRoles().isEmpty()) {
+            for (RoleDTO roleDTO : dto.getRoles()) {
+                if (roleDTO.getId() != null) {
+                    Optional<Role> obj = roleRepository.findById(roleDTO.getId());
+                    Role role = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + roleDTO.getId()));
+                    entity.getRoles().add(role);
+
+                } else {
+
+                    Role newRole = new Role();
+                    newRole.setAuthority(roleDTO.getAuthority());
+                    roleRepository.save(newRole);
+
+                    entity.getRoles().add(newRole);
+                }
+            }
+        }
+    }
+}
+
