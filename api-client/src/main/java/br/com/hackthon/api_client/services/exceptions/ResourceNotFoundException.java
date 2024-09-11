621e4a2ae93d43c112830f06c7c6e091a7420615
IgorTecnologia <iguigu2019@gmail.com>
2024-09-11 05:29:59 -0300

    Resource Not Found Exception

---
+++ api-client/src/main/java/br/com/hackthon/api_client/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.hackthon.api_client.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String msg){
+        super(msg);
+    }
+}
