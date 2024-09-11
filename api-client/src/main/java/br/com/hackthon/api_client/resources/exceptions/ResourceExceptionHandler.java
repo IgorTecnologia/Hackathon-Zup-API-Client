3341a4b948bf624c8a08df3d11b35a1c78f100ac
IgorTecnologia <iguigu2019@gmail.com>
2024-09-11 05:31:15 -0300

    Resource Exception Handler

---
+++ api-client/src/main/java/br/com/hackthon/api_client/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,51 @@
+package br.com.hackthon.api_client.resources.exceptions;
+
+import br.com.hackthon.api_client.services.exceptions.*;
+import jakarta.servlet.http.*;
+import org.springframework.http.*;
+import org.springframework.validation.*;
+import org.springframework.web.bind.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.*;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+
+        StandardError err = new StandardError();
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        err.setTimeStamp(Instant.now());
+        err.setError("Entity not found");
+        err.setMessage(e.getMessage());
+        err.setStatus(status.value());
+        err.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidatorError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+
+        ValidatorError err = new ValidatorError();
+
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+        err.setTimeStamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Validation Exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        for(FieldError f : e.getBindingResult().getFieldErrors()){
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(status).body(err);
+    }
+}
