97874b828f93eb498ce7eac8dd38201b7527cb95
IgorTecnologia <iguigu2019@gmail.com>
2024-09-11 05:15:46 -0300

    Authority Constraint Impl

---
+++ api-client/src/main/java/br/com/hackthon/api_client/validation/AuthorityConstraintImpl.java
@@ -0,0 +1,26 @@
+package br.com.hackthon.api_client.validation;
+
+import br.com.hackthon.api_client.repositories.*;
+import jakarta.validation.*;
+import org.springframework.beans.factory.annotation.*;
+
+public class AuthorityConstraintImpl implements ConstraintValidator<AuthorityConstraint, String> {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void initialize(AuthorityConstraint constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String authority, ConstraintValidatorContext context) {
+        if(authority == null || authority.trim().isEmpty() || authority.contains(" ")){
+            return true;
+        }
+
+        boolean existsByAuthority = roleRepository.existsByAuthority(authority);
+        return !existsByAuthority;
+    }
+}
