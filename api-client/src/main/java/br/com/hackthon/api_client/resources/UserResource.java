e923a3449ee545b25b11d616c49f168df72ab75e
IgorTecnologia <iguigu2019@gmail.com>
2024-09-11 05:34:53 -0300

    User Resource

---
+++ api-client/src/main/java/br/com/hackthon/api_client/resources/UserResource.java
@@ -0,0 +1,91 @@
+package br.com.hackthon.api_client.resources;
+
+import br.com.hackthon.api_client.dto.*;
+import br.com.hackthon.api_client.services.impl.*;
+import com.fasterxml.jackson.annotation.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.data.domain.*;
+import org.springframework.http.*;
+import org.springframework.validation.annotation.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserServiceImpl service;
+
+    @GetMapping
+    public ResponseEntity<Page<UserDTO>> findAllPaged(Pageable pageable){
+
+        Page<UserDTO> page = service.findAllPaged(pageable);
+        if(!page.isEmpty()){
+            for(UserDTO userDTO : page.toList()){
+                userDTO.add(linkTo(methodOn(UserResource.class).findById(userDTO.getId())).withSelfRel());
+            }
+        }
+        return ResponseEntity.ok().body(page);
+    }
+
+    @GetMapping(value = "/username/{username}")
+    public ResponseEntity<List<UserDTO>> findByUserName(@PathVariable String username){
+
+        List<UserDTO> list = service.findByUsername(username);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/email/{email}")
+    public ResponseEntity<List<UserDTO>> findByEmail(@PathVariable String email){
+
+        List<UserDTO> list = service.findByEmail(email);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/cpf/{cpf}")
+    public ResponseEntity<List<UserDTO>> findByCpf(@PathVariable String cpf){
+
+        List<UserDTO> list = service.findByCpf(cpf);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable UUID id){
+
+        UserDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> insert(@RequestBody UserDTO dto){
+
+        dto = service.insert(dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable UUID id, @RequestBody UserDTO dto){
+
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PutMapping("/password/{id}")
+    public ResponseEntity<Object> passwordUpdate(@PathVariable UUID id, @RequestBody UserDTO dto){
+
+        dto = service.passwordUpdate(id, dto);
+        return ResponseEntity.ok().body("Password update successfully.");
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Object> deleteById(@PathVariable UUID id){
+
+        service.deleteById(id);
+        return ResponseEntity.ok().body("User deleted successfully.");
+    }
+}
