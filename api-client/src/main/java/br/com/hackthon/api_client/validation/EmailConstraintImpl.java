2aa335cee320490d52660e6680f22e084094cacc
IgorTecnologia <iguigu2019@gmail.com>
2024-09-11 05:17:15 -0300

    Email Constraint Impl

---
+++ api-client/src/main/java/br/com/hackthon/api_client/validation/EmailConstraintImpl.java
@@ -0,0 +1,26 @@
+package br.com.hackthon.api_client.validation;
+
+import br.com.hackthon.api_client.repositories.*;
+import jakarta.validation.*;
+import org.springframework.beans.factory.annotation.*;
+
+public class EmailConstraintImpl implements ConstraintValidator<EmailConstraint, String> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void initialize(EmailConstraint constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        if(email == null || email.trim().isEmpty() || email.contains(" ")){
+            return true;
+        }
+
+        boolean existsByEmail = userRepository.existsByEmail(email);
+        return !existsByEmail;
+    }
+}
