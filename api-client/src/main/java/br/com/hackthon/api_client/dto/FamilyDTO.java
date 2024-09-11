6db0d679b8863e7383519f1b58c6ca2e9d28b61b
IgorTecnologia <iguigu2019@gmail.com>
2024-09-11 05:28:10 -0300

    Family DTO

---
+++ api-client/src/main/java/br/com/hackthon/api_client/dto/FamilyDTO.java
@@ -0,0 +1,79 @@
+package br.com.hackthon.api_client.dto;
+
+import br.com.hackthon.api_client.entities.*;
+import com.fasterxml.jackson.annotation.*;
+import jakarta.validation.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.springframework.hateoas.*;
+
+import java.time.*;
+import java.util.*;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class FamilyDTO extends RepresentationModel<FamilyDTO> {
+
+    private UUID id;
+
+    @NotNull(message = "Last name field is mandatory.")
+    @Size(min = 2, max = 70, message = "Minimum characters 2 and maximum 70.")
+    private String lastName;
+
+    @NotNull(message = "State field is mandatory.")
+    @Size(min = 2, max = 70, message = "Minimum characters 2 and maximum 70.")
+    private String state;
+
+    @NotNull(message = "City field is mandatory.")
+    @Size(min = 2, max = 70, message = "Minimum characters 2 and maximum 70.")
+    private String city;
+
+    @NotNull(message = "Street field is mandatory.")
+    @Size(min = 2, max = 170, message = "Minimum characters 2 and maximum 170.")
+    private String street;
+
+    @NotNull(message = "Address number field is mandatory.")
+    @Min(value = 1, message = "Minimum characters 1.")
+    @Max(value = 9999, message = "Maximum characters 9999")
+    private Integer addressNumber;
+
+    private LocalDateTime collectionDate;
+
+    private LocalDateTime lastUpdateDate;
+
+    @Valid
+    private List<PersonDTO> members = new ArrayList<>();
+
+    @Valid
+    private List<ChildrenDTO> children = new ArrayList<>();
+
+    public FamilyDTO(){
+    }
+
+    public FamilyDTO(Family entity){
+
+        id = entity.getId();
+        lastName = entity.getLastName();
+        state = entity.getState();
+        city = entity.getCity();
+        street = entity.getStreet();
+        addressNumber = entity.getAddressNumber();
+        collectionDate = entity.getCollectionDate();
+        lastUpdateDate = entity.getLastUpdateDate();
+
+    }
+
+    public FamilyDTO(Family entity, List<Person> members, List<Children> children){
+
+        this(entity);
+
+        if(members != null){
+            members.forEach(x -> this.members.add(new PersonDTO(x)));
+        }
+
+        if(children != null){
+            children.forEach(x -> this.children.add(new ChildrenDTO(x)));
+        }
+
+    }
+}
