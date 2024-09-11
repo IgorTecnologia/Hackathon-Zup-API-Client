463fc0e6a427379ba726baa36d3e5142181703d0
IgorTecnologia <iguigu2019@gmail.com>
2024-09-11 05:28:51 -0300

    Role DTO

---
+++ api-client/src/main/java/br/com/hackthon/api_client/dto/RoleDTO.java
@@ -0,0 +1,37 @@
+package br.com.hackthon.api_client.dto;
+
+import br.com.hackthon.api_client.entities.*;
+import br.com.hackthon.api_client.validation.*;
+import com.fasterxml.jackson.annotation.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.springframework.hateoas.*;
+
+import java.util.*;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+public class RoleDTO extends RepresentationModel<RoleDTO> {
+
+    public interface RoleView{
+        public static interface RegistrationPost { }
+        public static interface RolePut { }
+    }
+
+    private UUID id;
+
+    @Size(min = 4, max = 70, message = "Minimum characters 4 and maximum 70.")
+    @NotNull(message = "The authority field is mandatory.")
+    @AuthorityConstraint(message = "Invalid authority, already exists.")
+    private String authority;
+
+    public RoleDTO() {
+    }
+
+    public RoleDTO(Role entity) {
+
+        id = entity.getId();
+        authority = entity.getAuthority();
+    }
+}
+
