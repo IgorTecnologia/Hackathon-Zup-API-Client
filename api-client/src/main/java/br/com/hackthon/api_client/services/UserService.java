bcdcd9e2f1d3bb39023e2359f1471dfe5675b05a
IgorTecnologia <iguigu2019@gmail.com>
2024-09-11 05:23:08 -0300

    User Service

---
+++ api-client/src/main/java/br/com/hackthon/api_client/services/UserService.java
@@ -0,0 +1,27 @@
+package br.com.hackthon.api_client.services;
+
+import br.com.hackthon.api_client.dto.*;
+import org.springframework.data.domain.*;
+
+import java.util.*;
+
+public interface UserService {
+
+    Page<UserDTO> findAllPaged(Pageable pageable);
+
+    List<UserDTO>  findByUsername(String userName);
+
+    List<UserDTO> findByEmail(String email);
+
+    List<UserDTO> findByCpf(String cpf);
+
+    UserDTO findById(UUID id);
+
+    UserDTO insert(UserDTO dto);
+
+    UserDTO update(UUID id, UserDTO dto);
+
+    Object passwordUpdate(UUID id, UserDTO dto);
+
+    void deleteById(UUID id);
+}
