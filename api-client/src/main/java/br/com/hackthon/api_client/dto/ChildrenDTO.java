f7ce557c223a86020dc6b5577bad9be3e6043717
IgorTecnologia <iguigu2019@gmail.com>
2024-09-11 05:28:37 -0300

    Children DTO

---
+++ api-client/src/main/java/br/com/hackthon/api_client/dto/ChildrenDTO.java
@@ -0,0 +1,76 @@
+package br.com.hackthon.api_client.dto;
+
+import br.com.hackthon.api_client.entities.*;
+import br.com.hackthon.api_client.validation.*;
+import com.fasterxml.jackson.annotation.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.springframework.hateoas.*;
+
+import java.time.*;
+import java.util.*;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+public class ChildrenDTO extends RepresentationModel<ChildrenDTO> {
+
+    private UUID id;
+
+    @NotBlank(message = "First name field is mandatory.")
+    @Size(min = 2, max = 70, message = "Minimum characters 2 and maximum 70.")
+    private String firstName;
+
+    @NotNull(message = "Last name field is mandatory.")
+    @Size(min = 2, max = 70, message = "Minimum characters 2 and maximum 70.")
+    private String lastName;
+
+    @NotNull(message = "The date birth field is mandatory.")
+    private LocalDate dateBirth;
+
+    @NotNull(message = "Age field is mandatory and cannot be null.")
+    @Min(value = 1, message = "Minimum value 1.")
+    @Max(value = 170, message = "Maximum value 170.")
+    private Integer age;
+
+    @CpfConstraint(message = "Invalid, existing CPF.")
+    @NotNull(message = "CPF field is mandatory and cannot be null.")
+    @Size(min = 10, max = 20, message = "Minimum characters 10 and maximum 20.")
+    private String cpf;
+
+    private LocalDateTime collectionDate;
+
+    private FamilyDTO family;
+
+    public ChildrenDTO(){
+    }
+
+    public ChildrenDTO(Children entity){
+
+        id = entity.getId();
+        firstName = entity.getFirstName();
+        lastName = entity.getLastName();
+        dateBirth = entity.getDateBirth();
+        age = entity.getAge();
+        cpf = entity.getCpf();
+        collectionDate = entity.getCollectionDate();
+    }
+
+    public ChildrenDTO(Children entity, Family family) {
+
+        this(entity);
+
+        if (family != null) {
+            this.family = new FamilyDTO();
+            this.family.setId(family.getId());
+            this.family.setLastName(family.getLastName());
+            this.family.setState(family.getState());
+            this.family.setCity(family.getCity());
+            this.family.setStreet(family.getStreet());
+            this.family.setAddressNumber(family.getAddressNumber());
+
+            for(Children children : family.getChildren()){
+                this.family.getChildren().add(new ChildrenDTO(children));
+            }
+        }
+    }
+}
