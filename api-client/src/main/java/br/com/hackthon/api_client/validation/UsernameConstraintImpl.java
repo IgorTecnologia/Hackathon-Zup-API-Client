4a5beecc1cb1ae59d6dc7c2e53198b47e04c86cb
IgorTecnologia <iguigu2019@gmail.com>
2024-09-11 05:18:36 -0300

    Username Constraint Impl

---
+++ api-client/src/main/java/br/com/hackthon/api_client/validation/UsernameConstraintImpl.java
@@ -0,0 +1,26 @@
+package br.com.hackthon.api_client.validation;
+
+import br.com.hackthon.api_client.repositories.*;
+import jakarta.validation.*;
+import org.springframework.beans.factory.annotation.*;
+
+public class UsernameConstraintImpl implements ConstraintValidator<UsernameConstraint, String> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void initialize(UsernameConstraint constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String username, ConstraintValidatorContext context) {
+        if(username == null || username.trim().isEmpty() || username.contains(" ")){
+            return true;
+        }
+
+        boolean existsByUsername = userRepository.existsByUsername(username);
+        return !existsByUsername;
+    }
+}
