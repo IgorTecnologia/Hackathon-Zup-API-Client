bd903967095cca0f0b932349befe3fb60a259f3c
IgorTecnologia <iguigu2019@gmail.com>
2024-09-11 05:05:07 -0300

    Backend project created

---
+++ api-client/src/main/java/br/com/hackthon/api_client/ApiClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.hackthon.api_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClientApplication.class, args);
+	}
+
+}
---
+++ api-client/src/test/java/br/com/hackthon/api_client/ApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hackthon.api_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
