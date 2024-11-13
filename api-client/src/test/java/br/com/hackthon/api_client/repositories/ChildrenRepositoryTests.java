01481f21cee43a145813ea587a154e5c71042569
IgorTecnologia <iguigu2019@gmail.com>
2024-11-13 12:20:47 -0300

    Children Repository Tests

---
+++ api-client/src/test/java/br/com/hackthon/api_client/repositories/ChildrenRepositoryTests.java
@@ -0,0 +1,108 @@
+package br.com.hackthon.api_client.repositories;
+
+import br.com.hackthon.api_client.entities.*;
+import br.com.hackthon.api_client.services.exceptions.*;
+import br.com.hackthon.api_client.tests.*;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.autoconfigure.orm.jpa.*;
+import org.springframework.data.domain.*;
+
+import java.util.*;
+
+@DataJpaTest
+public class ChildrenRepositoryTests {
+
+    String firstName;
+    String lastName;
+    Integer age;
+    String cpf;
+
+    @BeforeEach
+    void setUp() throws Exception{
+
+        firstName = "Pedro";
+        lastName = "Pereira Almeida";
+        age = 18;
+        cpf = "832.092.230-50";
+    }
+
+    @Autowired
+    private ChildrenRepository repository;
+
+    @Test
+    public void findAllShouldReturnAllChildrenPaged(){
+
+        Pageable pageable = PageRequest.of(0, 12);
+
+        Page<Children> page = repository.findAll(pageable);
+
+        Assertions.assertNotNull(page);
+        Assertions.assertFalse(page.isEmpty());
+    }
+    @Test
+    public void findAllByFirstNameShouldReturnAllChildrenPagedByFirstNameWhenFirstNameExists(){
+
+        PageRequest pageable = PageRequest.of(0, 12);
+
+        Page<Children> page = repository.findAllPagedByFirstNameContainingIgnoreCase(firstName,pageable);
+
+        Assertions.assertNotNull(page);
+        Assertions.assertFalse(page.isEmpty());
+    }
+    
+    @Test
+    public void findAllByLastNameShouldReturnAllChildrenPagedByLastNameWhenLastNameExists(){
+
+        PageRequest pageable = PageRequest.of(0, 12);
+
+        Page<Children> page = repository.findAllPagedByLastNameContainingIgnoreCase(lastName, pageable);
+
+        Assertions.assertNotNull(page);
+        Assertions.assertFalse(page.isEmpty());
+    }
+
+    @Test
+    public void findAllByAgeShouldReturnAllChildrenPagedByAgeWhenAgeExists(){
+
+        PageRequest pageable = PageRequest.of(0, 12);
+
+        Page<Children> page = repository.findAllPagedByAge(age, pageable);
+
+        Assertions.assertNotNull(page);
+        Assertions.assertFalse(page.isEmpty());
+    }
+
+    @Test
+    public void findAllByCpfShouldReturnAllChildrenPagedByCpfWhenCpfExists(){
+
+        PageRequest pageable = PageRequest.of(0, 12);
+
+        Page<Children> page = repository.findAllPagedByCpfContainingIgnoreCase(cpf, pageable);
+
+        Assertions.assertNotNull(page);
+        Assertions.assertFalse(page.isEmpty());
+    }
+
+    @Test
+    public void insertShouldSaveObjectWhenCorrectStructure(){
+
+        Children entity = Factory.createdChildren();
+
+        repository.save(entity);
+
+        Assertions.assertEquals(10, repository.count());
+    }
+
+    @Test
+    public void deleteByIdShouldDeleteObjectWhenIdExists(){
+
+        Optional<Children> obj = repository.findAll().stream().findFirst();
+
+        UUID id = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
+
+        repository.deleteById(id);
+
+        Assertions.assertEquals(8, repository.count());
+    }
+}
