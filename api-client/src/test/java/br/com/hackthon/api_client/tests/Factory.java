865c92d3ffc47dfe7bb34fad73e85bd6766461ae
IgorTecnologia <iguigu2019@gmail.com>
2024-11-15 10:21:07 -0300

    Factory

--- api-client/src/test/java/br/com/hackthon/api_client/tests/Factory.java
+++ api-client/src/test/java/br/com/hackthon/api_client/tests/Factory.java
@@ -4,6 +4,7 @@ import br.com.hackthon.api_client.dto.*;
 import br.com.hackthon.api_client.entities.*;
 import br.com.hackthon.api_client.repositories.*;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.cglib.core.*;
 
 import java.time.*;
 import java.util.*;
@@ -22,20 +23,13 @@ public class Factory {
         return entity;
     }
 
-    public static Family createdFamily() {
+    public static PersonDTO createdPersonDtoToInsert() {
 
         UUID id = UUID.randomUUID();
 
-        Family entity = new Family(id, "Braga Luiz", "SP", "São Paulo", "Soares de Lima", 77, LocalDateTime.now());
-        return entity;
-    }
+        PersonDTO dto = new PersonDTO(id, "Luana", "Fernanda Souza", LocalDate.of(1998, 1, 1), 26, "123.244.300-30", 60000.00, LocalDateTime.now());
 
-    public static Children createdChildren(){
-
-        UUID id = UUID.randomUUID();
-
-        Children entity = new Children(id, "Lucas", "Guitierres", LocalDate.of(2005, 1 , 1), 19, "123.456.789-11", LocalDateTime.now());
-        return entity;
+        return dto;
     }
 
     public static PersonDTO createdPersonDtoUpdate() {
@@ -47,13 +41,12 @@ public class Factory {
         return dto;
     }
 
-    public static PersonDTO createdPersonDtoToInsert() {
+    public static Family createdFamily() {
 
         UUID id = UUID.randomUUID();
 
-        PersonDTO dto = new PersonDTO(id, "Luana", "Fernanda Souza", LocalDate.of(1998, 1, 1), 26, "123.244.300-30", 60000.00, LocalDateTime.now());
-
-        return dto;
+        Family entity = new Family(id, "Braga Luiz", "SP", "São Paulo", "Soares de Lima", 77, LocalDateTime.now());
+        return entity;
     }
 
     public static FamilyDTO createdFamilyDto(){
@@ -64,6 +57,30 @@ public class Factory {
 
         return dto;
 
+    }
+
+    public static Children createdChildren(){
+
+        UUID id = UUID.randomUUID();
+
+        Children entity = new Children(id, "Lucas", "Guitierres", LocalDate.of(2005, 1 , 1), 19, "123.456.789-11", LocalDateTime.now());
+        return entity;
+    }
+
+    public static ChildrenDTO createdChildrenDtoToInsert(){
+
+        UUID id = UUID.randomUUID();
+
+        ChildrenDTO dto = new ChildrenDTO(id, "Albert", "Souza", LocalDate.of(2005, 2,2), 19, "742.218.630-57", LocalDateTime.now());
+        return dto;
+    }
+
+    public static ChildrenDTO createdChildrenDtoToUpdate(){
+
+        UUID id = UUID.randomUUID();
+
+        ChildrenDTO dto = new ChildrenDTO(id, "Francisco", "Ferraz", LocalDate.of(2004, 3,3), 20, "742.218.630-58", LocalDateTime.now());
+        return dto;
     }
 }
 
