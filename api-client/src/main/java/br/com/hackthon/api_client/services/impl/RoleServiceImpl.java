80bb91c8e2d6cdbe9ad9490cee11e863770c5463
IgorTecnologia <iguigu2019@gmail.com>
2024-11-22 17:29:51 -0300

    Role Service Impl

--- api-client/src/main/java/br/com/hackthon/api_client/services/impl/RoleServiceImpl.java
+++ api-client/src/main/java/br/com/hackthon/api_client/services/impl/RoleServiceImpl.java
@@ -6,6 +6,7 @@ import br.com.hackthon.api_client.repositories.*;
 import br.com.hackthon.api_client.services.*;
 import br.com.hackthon.api_client.services.exceptions.*;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.dao.*;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.*;
 import org.springframework.transaction.annotation.*;
@@ -21,7 +22,7 @@ public class RoleServiceImpl implements RoleService {
 
     @Transactional(readOnly = true)
     @Override
-    public Page<RoleDTO> findAllPaged(Pageable pageable){
+    public Page<RoleDTO> findAllPaged(Pageable pageable) {
 
         Page<Role> page = repository.findAll(pageable);
         return page.map(RoleDTO::new);
@@ -32,13 +33,15 @@ public class RoleServiceImpl implements RoleService {
     public Page<RoleDTO> findAllPagedByAuthority(String authority, Pageable pageable) {
 
         Page<Role> page = repository.findAllByAuthorityContainingIgnoreCase(authority, pageable);
-
+        if (page.isEmpty()) {
+            throw new ResourceNotFoundException("Authority not found: " + authority);
+        }
         return page.map(RoleDTO::new);
     }
 
     @Transactional(readOnly = true)
     @Override
-    public RoleDTO findById(UUID id){
+    public RoleDTO findById(UUID id) {
 
         Optional<Role> obj = repository.findById(id);
         Role entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + id));
@@ -46,7 +49,7 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
-    public RoleDTO insert(RoleDTO dto){
+    public RoleDTO insert(RoleDTO dto) {
 
         Role entity = new Role();
 
@@ -56,7 +59,7 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
-    public RoleDTO update(UUID id, RoleDTO dto){
+    public RoleDTO update(UUID id, RoleDTO dto) {
 
         Optional<Role> obj = repository.findById(id);
         Role entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + id));
@@ -67,13 +70,17 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
-    public void deleteById(UUID id){
+    public void deleteById(UUID id) {
 
+        try {
         Optional<Role> obj = repository.findById(id);
-        if(obj.isEmpty()){
+        if (obj.isEmpty()) {
             throw new ResourceNotFoundException("Id not found: " + id);
         }
 
-        repository.deleteById(id);
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException("Integrity violation Exception");
+        }
     }
 }
