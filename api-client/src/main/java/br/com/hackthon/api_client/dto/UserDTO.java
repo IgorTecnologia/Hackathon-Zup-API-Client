797af681bba62ab33933ca8686d3621838cd6c3d
IgorTecnologia <iguigu2019@gmail.com>
2024-12-13 10:33:09 -0300

    User DTO

--- api-client/src/main/java/br/com/hackthon/api_client/dto/UserDTO.java
+++ api-client/src/main/java/br/com/hackthon/api_client/dto/UserDTO.java
@@ -3,7 +3,6 @@ package br.com.hackthon.api_client.dto;
 import br.com.hackthon.api_client.entities.*;
 import br.com.hackthon.api_client.validation.*;
 import com.fasterxml.jackson.annotation.*;
-import jakarta.validation.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
 import org.springframework.hateoas.*;
@@ -23,27 +22,27 @@ public class UserDTO extends RepresentationModel<UserDTO> {
 
     private UUID id;
 
-    @Size(min = 4, max = 50, message = "Minimum characters 4 and maximum 30.", groups = UserView.registrationPost.class)
-    @NotBlank(message = "The user name field is mandatory.", groups = UserView.registrationPost.class)
+    @Size(min = 4, max = 50, message = "Minimum characters 4 and maximum 50.", groups = UserView.registrationPost.class)
+    @NotBlank(message = "The username field is mandatory and blanks are not allowed.", groups = UserView.registrationPost.class)
+    @UsernameConstraint(message = "Invalid username, already exists.", groups = UserView.registrationPost.class)
     @JsonView({UserView.registrationPost.class})
-    @UsernameConstraint(groups = UserView.registrationPost.class, message = "Invalid username, already exists.")
     private String username;
 
     @Size(min = 10, max = 50, message = "Minimum characters 10 and maximum 50.", groups = UserView.registrationPost.class)
     @Email(message = "The email field must follow an acceptable standard.", groups = UserView.registrationPost.class)
-    @NotNull(message = "The e-mail field is mandatory.", groups = UserView.registrationPost.class)
+    @NotBlank(message = "The e-mail field is mandatory and blanks are not allowed.", groups = UserView.registrationPost.class)
     @EmailConstraint(message = "Invalid email, already exists.", groups = UserView.registrationPost.class)
     @JsonView(UserView.registrationPost.class)
     private String email;
 
     @Size(min = 7, max = 70, message = "Minimum characters 7 and maximum 70.", groups = {UserView.registrationPost.class, UserView.passwordPut.class})
-    @NotBlank(message = "The password field is mandatory.", groups = {UserView.registrationPost.class, UserView.passwordPut.class})
+    @NotBlank(message = "The password field is mandatory and blanks are not allowed.", groups = {UserView.registrationPost.class, UserView.passwordPut.class})
     @JsonView({UserView.registrationPost.class, UserView.passwordPut.class})
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
-    @Size(min = 7, max = 70, groups = UserView.passwordPut.class)
-    @NotBlank(message = "The Old password field is mandatory.", groups = UserView.passwordPut.class)
+    @Size(min = 7, max = 70, message = "Minimum characters 7 and maximum 70.", groups = UserView.passwordPut.class)
+    @NotBlank(message = "The Old password field is mandatory and blanks are not allowed.", groups = UserView.passwordPut.class)
     @JsonView(UserView.passwordPut.class)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String oldPassword;
@@ -53,11 +52,12 @@ public class UserDTO extends RepresentationModel<UserDTO> {
     @JsonView({UserView.registrationPost.class, UserView.userPut.class})
     private String fullName;
 
-    @Size(min = 9, max = 30, message = "Minimum characters 9 and maximum 20.", groups = {UserView.registrationPost.class, UserView.userPut.class})
+    @Size(min = 10, max = 20, message = "Minimum characters 9 and maximum 20.", groups = {UserView.registrationPost.class, UserView.userPut.class})
     @JsonView({UserView.registrationPost.class, UserView.userPut.class})
     private String phoneNumber;
 
-    @Size(min = 10, max = 30, message = "Minimum characters 10 and maximum 30.", groups = {UserView.registrationPost.class, UserView.userPut.class})
+    @Size(min = 10, max = 20, message = "Minimum characters 10 and maximum 20.", groups = {UserView.registrationPost.class, UserView.userPut.class})
+    @NotBlank(message = "The CPF field is mandatory and blanks are not allowed.", groups = {UserView.registrationPost.class, UserView.userPut.class})
     @CpfConstraint(message = "Invalid CPF, already exists.", groups = {UserView.registrationPost.class, UserView.userPut.class})
     @JsonView({UserView.registrationPost.class, UserView.userPut.class})
     private String cpf;
@@ -71,7 +71,6 @@ public class UserDTO extends RepresentationModel<UserDTO> {
     @JsonView({UserView.registrationPost.class, UserView.userPut.class})
     private LocalDateTime lastUpdateDate;
 
-    @Valid
     @JsonView({UserView.registrationPost.class, UserView.userPut.class})
     private List<RoleDTO> roles = new ArrayList<>();
 
